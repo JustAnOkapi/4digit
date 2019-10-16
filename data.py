69998c1e123c384a451bf7509f379d7a2e85de3f
JustAnOkapi <justanokapi@gmail.com>
2019-10-16 15:26:19 -0500

    .

--- data.py
+++ data.py
@@ -5,23 +5,27 @@ def legal(inp):
     if '0' not in inp: # if the is no zero
         return bool(len(inp) == len(set(inp))) # if all numbers are not repeating
 
+all_raw = [] # all legal numbers
 current = 1234 # lowest posible number
-all_raw = [] # create non-formatted data
+
 while len(str(current)) == 4: # while number is 4 digits
     if legal(current): # check legality
         all_raw.append(current) # add number to the all_raw list
-        current += 1 # move to next number
-    else: # if not legal
-        current += 1 # skip to next number
+    current += 1 # move to next number
 
-data = {} # create formatted data
+BvC = [(4,0), (2,2), (1,3), (0,4), (3,0), (2,1), (1,2), (0,3), (2,0), (1,1), (0,2), (1,0), (0,1), (0,0)] # all BULL to COW ratios
 
+guess = 1234 # the first guess
+BC1r = input(f"{guess}>>>") # ask the result and store it raw
+BC1rt = BC1r.split(",") # format the raw into a temp list
+BC1 = (BC1rt[0],BC1rt[1]) # format the temp into a perm tuple
+
+guess_split = [str(guess)[0], str(guess)[1], str(guess)[2], str(guess)[3]] # turn int into a list of spilits
+
+pos1 = [] # possible numbers after first guess
 for current in all_raw:
-    for current2 in all_raw:
-        continue # i dont know what i was doing
-
-print(all_raw)
-print(data)
-
+    if 1==1:
+        if 1==1: # temp
+            pos1.append(current)
 
 # https://en.wikipedia.org/wiki/Bulls_and_Cows
