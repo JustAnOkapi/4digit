5851719b9e5bcd84ceb418d030a64825f80a0283
JustAnOkapi <JustAnOkapi@gmail.com>
2019-10-15 23:04:37 -0500

    Clean-up

--- data.py
+++ data.py
@@ -1,30 +1,27 @@
 #import json
 
-def legal(input):
-    input = list(str(input))
-    if '0' not in input: 
-        if len(input) == len(set(input)):
-            return True
-        else: 
-            return False
-    else: 
-        return False
+def legal(inp):
+    inp = list(str(inp)) # make each digit into a str in a list
+    if '0' not in inp: # if the is no zero
+        return bool(len(inp) == len(set(inp))) # if all numbers are not repeating
 
-current = 1234
-all_raw = []
-while len(str(current)) == 4:
-    if legal(current):
-        all_raw.append(current)
-        current += 1
-    else:
-        current += 1
-        continue
+current = 1234 # lowest posible number
+all_raw = [] # create non-formatted data
+while len(str(current)) == 4: # while number is 4 digits
+    if legal(current): # check legality
+        all_raw.append(current) # add number to the all_raw list
+        current += 1 # move to next number
+    else: # if not legal
+        current += 1 # skip to next number
 
-data = {}
+data = {} # create formatted data
 
 for current in all_raw:
     for current2 in all_raw:
-        
+        continue # i dont know what i was doing
 
 print(all_raw)
 print(data)
+
+
+# https://en.wikipedia.org/wiki/Bulls_and_Cows
